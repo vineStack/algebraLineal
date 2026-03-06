298c8a2b790e6e3985128d81dc89d72a1c32db01
vineStack <perez.lopez.jesus.eduardo0@gmail.com>
2026-03-06 17:29:33 -0600

    Matrix zero nxn added and change to no-void in mkSqMatrixI

--- ioMatriz.cpp
+++ ioMatriz.cpp
@@ -2,13 +2,18 @@
 #include <vector>
 using namespace std;
 
+vector<vector<double>> mkSqMatrixZero(int n){
+    vector<vector<double>> A(n, vector<double>(n,0.0));
+    return A;
+}
+
 vector<vector<double>> mkSqMatrixI(int n){
-    vector<vector<double>> matriz(n, vector<double>(n,0.0));
+    vector<vector<double>> A = mkSqMatrixZero(n);
     for (int i = 0; i < n; i++)
     {
-        matriz[i][i] = 1;
+        A[i][i] = 1;
     }
-    return matriz;
+    return A;
 }
 
 void imprimirMatriz(const vector<vector<double>>& m, int n){
