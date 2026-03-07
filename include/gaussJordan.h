7c080b8e28751b17d072ab7794772e5a62a24392
vineStack <perez.lopez.jesus.eduardo0@gmail.com>
2026-03-06 20:18:34 -0600

    first steps to gauss-Jordan method

---
+++ include/gaussJordan.h
@@ -0,0 +1,8 @@
+#ifndef gaussJordan_h
+#define gaussJordan_h
+
+#include <vector>
+
+std::vector<std::vector<double>> multiplicacion(std::vector<std::vector<double>> &A, std::vector<std::vector<double>> &B, int n, int m, int p);
+
+#endif
---
+++ src/gaussJordan.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+vector<vector<double>> multiplicacion(vector<vector<double>> &A, vector<vector<double>> &B, int n, int m, int p)
+{
+    vector<vector<double>> C(n, vector<double>(p,0.0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            for (int k = 0; k < m; k++)
+            {
+                C[i][j] += A[i][k]*B[k][j];
+            }
+        }
+    }
+
+    return C;
+}
